98cdf210572a438d384c93aa95be0c109834dbab
HelgaVictorovna <147982475+HelgaVictorovna@users.noreply.github.com>
2024-01-17 20:31:05 +0300

    Add files via upload

---
+++ Lesson 2.76/main.js
@@ -0,0 +1,10 @@
+const firstName = 'Ольга'
+const secondName = 'Храброва'
+let age = 'опять18'
+const placeOfBirth = 'Москва'
+let colour = 'Блондинка'
+
+console.log(firstName, secondName, age, 'лет', placeOfBirth, colour)
+
+
+// let объвляет переменную, которую в любой момент можно изменить, const объявляет переменную, которую нельзя изменить

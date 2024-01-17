f03ed0d67bccff5e394abe3025a3db947b9c7060
HelgaVictorovna <147982475+HelgaVictorovna@users.noreply.github.com>
2024-01-17 20:32:32 +0300

    Add files via upload

---
+++ Lesson 2.80/main.js
@@ -0,0 +1,13 @@
+
+let age = '42'
+
+if(age >= 18) {
+    console.log('Вы совершеннолетний')
+} else{
+    console.log('Вам еще нет 18')
+}
+
+
+let years = '16' 
+
+years >= 18? console.log('Вы совершеннолетний'): console.log('Вам еще нет 18')

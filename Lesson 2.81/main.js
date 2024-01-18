648b035c93ef449f3d8e9a5216091054bf0904b9
HelgaVictorovna <147982475+HelgaVictorovna@users.noreply.github.com>
2024-01-18 17:27:09 +0300

    Add files via upload

---
+++ Lesson 2.81/main.js
@@ -0,0 +1,21 @@
+
+
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let i=0; i<numbers.length; i=i+2) {
+    console.log(numbers[i] + 1)
+}
+
+
+
+
+
+
+
+
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'dark blue', 'purple']
+for (let i=6; i>=0; i=i-1) {
+    console.log(colors[i])
+}

622575e7109adb4cafee21b1c5c34a2e35fe0804
HelgaVictorovna <147982475+HelgaVictorovna@users.noreply.github.com>
2024-02-08 10:13:46 +0300

    Add files via upload

---
+++ Lesson 2.83/main.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+const looser = {
+  Eva: {
+    age: 25,
+    isAdmin: true,
+    sayHello(name) {
+      console.log(`Hello ${name}`)
+    }
+
+  }
+
+}
+console.log(looser.Eva)
+looser.Eva.sayHello('Max')
+
+
+
+const users = [
+  {
+    name: 'Olga',
+    age: 42,
+    isAdmin: true
+  },
+  {
+    name: 'Ivan',
+    age: 62,
+    isAdmin: false
+  },
+  {
+    name: 'Mike',
+    age: 44,
+    isAdmin: false
+  },
+  {
+    name: 'Alex',
+    age: 32,
+    isAdmin: false
+  }
+]
+
+for (let i = 0; i <= users.length; i++) {
+  let notAdmin=0;
+  if (isAdmin === false) {
+    notAdmin = notAdmin + 1
+  }
+  
+}
+console.log(notAdmin)
+
